5735610239dfb7cc142e8c53baa0114d8b97172b
Damian Boborzi <damian.boborzi@uni-a.de>
2024-05-16 15:43:26 +0200

    added some objaverse notebooks

---
+++ start.sh
@@ -0,0 +1,3 @@
+python3 scripts/distributed.py --num_gpus 1 --workers_per_gpu 1 --input_models_path "./input_models_path.json"
+
+python scripts/blender_script.py --object_path="https://huggingface.co/datasets/allenai/objaverse/resolve/main/glbs/000-032/bb7a8d3861f04ab68d0e09120ce11f3a.glb"
